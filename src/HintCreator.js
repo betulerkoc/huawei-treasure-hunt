0ae8cbd7e159c74319cf61a194181993b6dfe414
Betul Fatma Erkoc <betul.fatma.erkoc1@huawei.com>
2021-05-18 14:32:39 +0300

    update hints

--- src/HintCreator.js
+++ src/HintCreator.js
@@ -12,27 +12,14 @@ import FilePickerManager from 'react-native-file-picker';
 const hintData = ["What has 4 legs but can’t walk and has hair at night? You saw it this morning. The next clue is hiding under it.",
   "It is white but it turns yellow when falls on the floor. You next clue is hiding right next to it.",
   "I lose my head in the mornings and get it back at nights. What am I? The next clue is hiding under it.",
-  "I stand up and make your day brighter.",
-  "What has 4 legs but can’t walk and has hair at night? You saw it this morning. The next clue is hiding under it.",
-  "It is white but it turns yellow when falls on the floor. You next clue is hiding right next to it.",
-  "I lose my head in the mornings and get it back at nights. What am I? The next clue is hiding under it.",
   "I stand up and make your day brighter."]
 
 const App = () => {
 
   const base64ImagePng = "data:image/png;base64,";
 
-  const [base64ImageData, setImageData] = useState(null);
-  const [reqObj, setReqObj] = useState({
-    content: "",
-    type: ScanPlugin.ScanType.All,
-    width: 200,
-    height: 200,
-    margin: 1,
-    color: -16777216, // BLACK,
-    backgroundColor: -1, // WHITE,
-  })
-//    qrLogoBitmap: "content://com.android.providers.media.documents/document/image%3A76981"
+  const [imageData, setImageData] = useState([]);
+
   const filePicker = (callback) => {
     FilePickerManager.showFilePicker(null, (response) => {
       console.log("Response = ", response);
@@ -46,41 +33,47 @@ const App = () => {
     });
   }
 
-  const buildBitmap = async () => {
-    filePicker((response) => {   
-      setReqObj({
-        qrLogoBitmap: response.uri
-      })
-      console.log(reqObj)
-     })
+  const buildBitmap = () => {
+    filePicker((response) => {
+      let args;
+      let id = 0
+      hintData.map((x) => {
+        args = {
+          content: x,
+          type: ScanPlugin.ScanType.All,
+          width: 200,
+          height: 200,
+          margin: 1,
+          color: -16777216,
+          backgroundColor: -1,
+          showImage: false,
+          qrLogoBitmap: response.uri
+        };
 
-    hintData.map((x) => {
-      console.log(reqObj)
-      setReqObj({
-        ...reqObj,
-        content: x
+        ScanPlugin.Utils.buildBitmap(args)
+          .then((res) => {
+            setImageData([...imageData, {
+              id: id + 1,
+              bitmap: res
+            }]);
+            console.log(imageData.length)
+          })
+          .catch((e) => console.log(e))
       })
-      ScanPlugin.Utils.buildBitmap(reqObj)
-      .then((res) => {
-        console.log(res);  
-        setImageData([...base64ImageData, res])
-      })
-      .catch((e) => { console.log(e) })
-    });
-
-
+    })
   }
   return (
     <View>
       <TouchableOpacity onPress={buildBitmap}>
         <Text>Build bitmap</Text>
       </TouchableOpacity>
-      {base64ImageData && <Image
-        style={{ height: 300 }}
-        source={{
-          uri: base64ImagePng + base64ImageData,
-        }}
-      />}
+      {console.log("yeys " + JSON.stringify(imageData))}
+      {imageData &&
+        <View>
+          <Image style={{ height: 300, width: 300 }}
+            source={{ uri: base64ImagePng + imageData }} />
+        </View>
+      }
 
     </View>
   );
